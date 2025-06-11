cc8c7b75ace298774caf31a5ee82bc326e3a0b3a
andreeaelenasamur <andelenasamur@gmail.com>
2025-06-11 15:53:10 +0200

    Separate logic in dragonball-super component in a two new components

---
+++ src/app/components/dragonball/character-add/character-add.component.ts
@@ -0,0 +1,39 @@
+import { Component, signal } from '@angular/core';
+import { Character } from '../../../interfaces/character.interface';
+
+@Component({
+  selector: 'dragonball-character-add',
+  templateUrl: './character-add.component.html',
+})
+export class CharacterAddComponent {
+
+  name = signal('');
+  power = signal(0);
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 }
+  ]);
+
+  addCharacter() {
+    if( !this.name() || !this.power() || this.power() <= 0 ) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters.update((list) => [...list, newCharacter]);
+    console.log({newCharacter});
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
+}
--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
 
 interface Character {
   id: number;
@@ -8,37 +10,17 @@ interface Character {
 
 @Component({
   selector: 'dragonball-super',
-  templateUrl: './dragonball-super-page.component.html'
+  templateUrl: './dragonball-super-page.component.html',
+  imports: [
+    CharacterListComponent,
+    CharacterAddComponent
+  ]
 })
 export class DragonballSuperPageComponent {
-
-  name = signal('');
-  power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 }
   ]);
-
-  addCharacter() {
-    if( !this.name() || !this.power() || this.power() <= 0 ) {
-      return;
-    }
-
-    const newCharacter: Character = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power(),
-    };
-
-    this.characters.update((list) => [...list, newCharacter]);
-    this.resetFields();
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
-
 
 }
